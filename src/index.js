f5e83045ef7918d2fee60d51f5970e044a5d9ebb
Alyssa20lopez <alyssa20lopez@gmail.com>
2022-10-21 11:39:07 -0400

    Added Typography to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import Typography from './styles/Typography';
 
 
 
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Typography />
       <App />
     </BrowserRouter>
   </React.StrictMode>
